e1158c3a0a77139f5e1fe1c5e80d7a65b2693483
robonen <robonenandrew@gmail.com>
2023-03-15 20:52:11 +0700

    feat(artists): m:m artists and books

---
+++ app/Models/ArtistBook.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class ArtistBook extends Pivot
+{
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'artists_books';
+}
