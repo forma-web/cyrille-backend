a4218046c6290f326cf1a7be524aedefcd30edb3
robonen <robonenandrew@gmail.com>
2023-03-15 20:35:09 +0700

    feat(artists): model and factory

---
+++ database/factories/ArtistFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Artist>
+ */
+class ArtistFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'avatar' => $this->fakeAvatar(),
+        ];
+    }
+
+    private function fakeAvatar(): string
+    {
+        return 'https://i.pravatar.cc/300?u='.Str::random(10);
+    }
+}
