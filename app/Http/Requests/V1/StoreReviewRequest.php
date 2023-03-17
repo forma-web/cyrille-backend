86dcc6fb610545a8364e45e63c7c84db373ace6e
robonen <robonenandrew@gmail.com>
2023-03-17 13:57:29 +0700

    feat(reviews): model and migration

---
+++ app/Http/Requests/V1/StoreReviewRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+class StoreReviewRequest extends BaseFormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'rating' => ['required', 'integer', 'min:1', 'max:5'],
+            'comment' => ['string', 'max:1000'],
+        ];
+    }
+}
