ddf188c2899533067ec594dcaa42981d67b85de5
robonen <robonenandrew@gmail.com>
2023-03-29 21:01:15 +0700

    feat(db): indices

--- database/migrations/2023_03_15_165907_create_reviews_table.php
+++ database/migrations/2023_03_15_165907_create_reviews_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(Book::class)
+                ->index()
                 ->constrained()
                 ->cascadeOnUpdate()
                 ->cascadeOnDelete();
--- database/migrations/2023_03_24_064000_create_chapters_table.php
+++ database/migrations/2023_03_24_064000_create_chapters_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
         Schema::create('chapters', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(Book::class)
+                ->index()
                 ->constrained()
                 ->cascadeOnUpdate()
                 ->cascadeOnDelete();
