13389e15825943c775c72603b220bbe06b439780
robonen <robonenandrew@gmail.com>
2023-07-06 05:01:25 +0700

    fix(otp): model

---
+++ app/Models/Otp.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\OtpTypesEnum;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Otp extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'type',
+        'code',
+        'expires_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'type' => OtpTypesEnum::class,
+        'expires_at' => 'datetime',
+        'used_at' => 'datetime',
+    ];
+}
---
+++ database/migrations/2023_07_05_213624_create_otps_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Enums\OtpTypesEnum;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('otps', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)
+                ->index()
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->enum('type', OtpTypesEnum::values());
+            $table->string('code', 10);
+            $table->timestamp('expires_at');
+            $table->timestamp('used_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('otps');
+    }
+};
