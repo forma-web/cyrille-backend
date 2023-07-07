db9b53fbd40f74cc33da15dac04be1a403f22854
robonen <robonenandrew@gmail.com>
2023-07-08 00:21:24 +0700

    feat(otp): remove default laravel timestamps

--- app/Models/Otp.php
+++ app/Models/Otp.php
@@ -6,9 +6,16 @@ use App\Enums\OtpTypesEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Otp extends Model
+final class Otp extends Model
 {
     use HasFactory;
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
 
     /**
      * The attributes that are mass assignable.
