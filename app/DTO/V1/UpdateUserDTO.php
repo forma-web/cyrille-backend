b4d69dbb82f2c0761b9015e608686a9372828ec2
robonen <robonenandrew@gmail.com>
2023-07-17 04:45:28 +0700

    refactor(dto): update user and abstract dto

--- app/DTO/V1/UpdateUserDTO.php
+++ app/DTO/V1/UpdateUserDTO.php
@@ -7,6 +7,9 @@ final readonly class UpdateUserDTO extends DTO
     public function __construct(
         public ?string $name = null,
         public ?string $email = null,
+        public ?string $password = null,
+        public ?string $password_confirmation = null,
+        public ?string $current_password = null,
     ) {
     }
 }
